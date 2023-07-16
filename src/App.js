a841dabe4a886bb8e590fff0e820cbbaff706775
“vukas86” <vukasbit@gmail.com>
2023-07-16 19:02:15 +0200

    working on footer part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Works from "./pages/Works";
 import "./App.css";
 import RootLayout from "./pages/Root";
+import ScrollToTop from "./components/ScrollToTop";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +29,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  // const { pathname } = useLocation();
+  // useEffect(() => {
+  //   document.documentElement.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  // }, [pathname]);
+
   return <RouterProvider router={router} />;
 }
 
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,16 +1,42 @@
 import { Link } from "react-router-dom";
 import styles from "./ContactUs.module.css";
+import linkIcon from "../assets/icons/lnk.svg";
+import githubIcon from "../assets/icons/github.svg";
 
 function ContactUs() {
   return (
     <>
       <div className={styles.contactContainer}>
-        <h2>
-          Unlock Your Brand's Potential. <br /> Connect with Us Today!
-        </h2>
-        <Link to="/contact">
-          Contact Us <span>&#8594;</span>
-        </Link>
+        <section>
+          <h2>
+            Unlock Your Brand's Potential. <br /> Connect with Us Today!
+          </h2>
+          <Link to="/contact">
+            Contact Us <span>&#8594;</span>
+          </Link>
+        </section>
+        <section className={styles.addresses}>
+          <p>
+            create with passion by aleksandarvukasovic © 2023 all rights
+            reserved
+          </p>
+          <div className={styles.socialLinks}>
+            <a
+              href="https://www.linkedin.com/in/aleksandar-vukasovic"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={linkIcon} alt="linkendin logo" />
+            </a>
+            <a
+              href="https://github.com/vukas86"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src={githubIcon} alt="github logo" />
+            </a>
+          </div>
+        </section>
       </div>
     </>
   );
