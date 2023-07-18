58a3ed33e9fc07dc2fb13e2e674c7e809ea1c840
“vukas86” <vukasbit@gmail.com>
2023-07-19 00:00:04 +0200

    Implement lazy-loading for images using Intersection Observer API in React

---
+++ src/hooks/LazyLoadImages.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+function LazyLoadImages() {
+  const [imgBlur, setImgBlur] = useState(true);
+
+  useEffect(() => {
+    const imgTargets = document.querySelectorAll("img[data-src");
+    const imgTargetRef = Array.from(imgTargets);
+
+    const loadImg = function (enteries, observer) {
+      enteries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.src = img.dataset.src;
+
+          img.addEventListener("load", () => {
+            setImgBlur(false);
+          });
+        }
+      });
+    };
+
+    const imgObserver = new IntersectionObserver(loadImg, {
+      root: null,
+      threshold: 0,
+    });
+
+    imgTargetRef.forEach((img) => imgObserver.observe(img));
+
+    return () => imgObserver.disconnect();
+  }, []);
+
+  const blurClass = imgBlur ? "lazyImg" : "";
+
+  return { blurClass };
+}
+
+export default LazyLoadImages;
